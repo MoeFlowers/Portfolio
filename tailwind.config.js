f55fb290a5a717ebd53d10026c4f919af0c27eba
MoeFlowers <moeflowers2@gmail.com>
2025-08-05 13:56:32 -0400

    feat:  add comunity service project

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,27 +1,13 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: "class",
-  content: [
-    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  darkMode: 'selector',
+  content: ["./src/**/*.{html,js,php}",
+    "../src/**/*.{html,js,php}",
   ],
   theme: {
     extend: {
       fontFamily: {
-        sans: ["var(--font-inter)"],
-        mono: ["var(--font-roboto-mono)"],
-      },
-      colors: {
-        gray: {
-          50: '#f9fafb',
-          100: '#f3f4f6',
-          200: '#e5e7eb',
-          300: '#d1d5db',
-          400: '#9ca3af',
-          500: '#6b7280',
-          600: '#4b5563',
-          700: '#374151',
-          800: '#1f2937',
-          900: '#111827',
-        },
+        roboto: ['Roboto', 'sans-serif'],
       },
     },
   },
