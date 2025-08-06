a6475bf0ecb0c3cb2e910ed5385a645d89615b9f
MoeFlowers <moeflowers2@gmail.com>
2025-08-05 22:42:26 -0400

    Updating data of Projects

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -5,20 +5,21 @@ interface Project {
   title: string;
   description: string;
   technologies: TechName[];
-  githubLink?: string; 
-  demoLink?: string; 
-  duration?: string; 
+  githubLink?: string;
+  demoLink?: string;
+  duration?: string;
 }
 
 export const projects: Project[] = [
   {
     id: 1,
     title: "AI-Powered Book Recommendation",
-    description: "Developed a collaborative filtering model (Scikitlearn) processing 10,000+ books. Integrated REST API (Flask/FastAPI) for real-time recommendations → 85% user-rated accuracy.",
+    description:
+      "Developed a collaborative filtering model (Scikitlearn) processing 10,000+ books. Integrated REST API (Flask/FastAPI) for real-time recommendations → 85% user-rated accuracy.",
     technologies: ["Python", "Scikit-learn", "FastAPI", "Flask"],
     githubLink: "https://github.com/MoeFlowers/Portfolio/tree/book",
     demoLink: "#",
-    duration: "2 months", 
+    duration: "2 months",
   },
   {
     id: 2,
@@ -26,19 +27,29 @@ export const projects: Project[] = [
     description:
       "Digitized 100% of patient records and automated billing, saving 6h/week in admin work. Built an appointment module with automated email reminders.",
     technologies: ["PHP", "JavaScript", "MySQL", "Tailwind CSS"],
-    githubLink: "#", 
-    demoLink: "#", 
+    githubLink: "https://github.com/MoeFlowers/Portfolio/tree/clinica",
+    demoLink: "#",
     duration: "6 months",
   },
   {
-    id: 2,
+    id: 3,
     title: "Full-Stack Web Developer — Community Service Project",
     description:
       "Led end-to-end development of a web app used by 2,000+ users, reducing data query time by 80%. Implemented secure CRUD operations with PHP/MySQL and responsive UI → 97% user satisfaction",
     technologies: ["PHP", "JavaScript", "MySQL", "Tailwind CSS"],
-    githubLink: "#", 
-    demoLink: "#", 
+    githubLink: "https://github.com/MoeFlowers/Portfolio/tree/comunity",
+    demoLink: "#",
     duration: "Jan 2024 – Jun 2024",
+  },
+  {
+    id: 4,
+    title: "Data Processing Bots | Python",
+    description:
+      "API-powered bots updating dashboards every 5 mins with Slack alerts. Automated repetitive tasks (~15h/week saved).",
+    technologies: ["Python",],
+    githubLink: "https://github.com/MoeFlowers/Portfolio/tree/comunity",
+    demoLink: "#",
+    duration: "4 months",
   },
   // Others
 ];
